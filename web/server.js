4a79759023601b0244d782b07fa9cff7e92ecdd1
toth-marcell <toth.marcell.21i@egressy.info>
2025-11-10 14:19:15 +0100

    web: start openapi docs

--- web/server.js
+++ web/server.js
@@ -6,6 +6,15 @@ dotenv.config({ quiet: true });
 
 const app = express();
 
+import { readFileSync } from "fs";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yaml";
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(YAML.parse(readFileSync("openapi.yaml", "utf-8")))
+);
+
 app.use(express.static("public"));
 app.use("/api", apiApp);
 
