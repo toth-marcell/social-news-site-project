ec24deb777360c2e3edfa9816f0f0b7f0f6a088a
toth-marcell <toth.marcell.21i@egressy.info>
2025-12-01 11:32:12 +0100

    web: set sitename in env, use it in openapi

--- web/server.js
+++ web/server.js
@@ -4,7 +4,7 @@ import apiRouter from "./api.js";
 import { exit } from "process";
 import dotenv from "dotenv";
 dotenv.config({ quiet: true });
-if (!(process.env.PORT && process.env.SECRET)) {
+if (!(process.env.PORT && process.env.SECRET, process.env.SITENAME)) {
   console.error("Not all required env variables set, see .env.example");
   exit(1);
 }
@@ -12,12 +12,18 @@ if (!(process.env.PORT && process.env.SECRET)) {
 const app = express();
 
 import { readFileSync } from "fs";
-import swaggerUI from "swagger-ui-express";
 import YAML from "yaml";
+const APISpec = YAML.parse(readFileSync("openapi.yaml", "utf-8"));
+APISpec.info.title = process.env.SITENAME + " API";
+app.get("/openapi.json", (req, res) => {
+  res.json(APISpec);
+});
+
+import swaggerUI from "swagger-ui-express";
 app.use(
   "/api-docs",
   swaggerUI.serve,
-  swaggerUI.setup(YAML.parse(readFileSync("openapi.yaml", "utf-8")))
+  swaggerUI.setup(null, { swaggerOptions: { url: "/openapi.json" } })
 );
 
 app.use(express.static("public"));
