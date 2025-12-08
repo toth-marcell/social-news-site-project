97dcaee7dcdd895df9e2796c9fc9abf8f2df4553
toth-marcell <toth.marcell.21i@egressy.info>
2025-12-08 14:19:20 +0100

    web: add basic generateTestData

---
+++ web/generateTestData.js
@@ -0,0 +1,30 @@
+import { faker } from "@faker-js/faker";
+import { User } from "./models.js";
+import { HashPassword } from "./auth.js";
+
+for (let i = 0; i < faker.number.int(1, 5); i++) {
+  const name = faker.internet.username();
+  const user = await User.create({
+    name,
+    password: HashPassword(name),
+    about: faker.person.bio(),
+  });
+  for (let j = 0; j < faker.number.int(2, 5); j++) {
+    let post;
+    if (faker.datatype.boolean()) {
+      post = await user.createPost({
+        title: faker.word.adjective() + " " + faker.word.noun(),
+        link: faker.internet.url(),
+        linkType: faker.helpers.arrayElement(["image", "website", "pdf"]),
+        category: faker.book.genre(),
+      });
+    } else {
+      post = await user.createPost({
+        title: faker.book.title(),
+        text: faker.lorem.paragraphs(2),
+        category: faker.book.genre(),
+      });
+    }
+    user.createComment({ PostId: post.id, text: "haha" });
+  }
+}
