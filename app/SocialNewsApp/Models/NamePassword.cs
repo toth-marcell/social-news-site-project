5a74a711418961a5dd4a104240024d5a776e94a9
toth-marcell <toth.marcell.21i@egressy.info>
2026-01-28 11:00:45 +0100

    app: add login/register view

---
+++ app/SocialNewsApp/Models/NamePassword.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialNewsApp.Models;
+
+public class NamePassword
+{
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
---
+++ app/SocialNewsApp/Views/LogInOrRegisterView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SocialNewsApp;
+
+public partial class LogInOrRegisterView : UserControl
+{
+    public LogInOrRegisterView()
+    {
+        InitializeComponent();
+    }
+}
