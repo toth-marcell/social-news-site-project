16a71a91ee0338c6cbd8408988c56d4d21b9b47b
toth-marcell <toth.marcell.21i@egressy.info>
2026-03-25 17:47:58 +0100

    app: show identicons

--- app/SocialNewsApp/Models/User.cs
+++ app/SocialNewsApp/Models/User.cs
@@ -1,12 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
-
+using Jdenticon;
 namespace SocialNewsApp.Models;
 
 public partial class UserName : ObservableObject
 {
     [ObservableProperty]
     string name;
+    public string Identicon => Jdenticon.Identicon.FromValue(Name, 25).ToSvg();
+    static UserName()
+    {
+        Jdenticon.Identicon.DefaultStyle = new()
+        {
+            BackColor = Jdenticon.Rendering.Color.Transparent
+        };
+    }
 }
 
 public partial class User : UserName
